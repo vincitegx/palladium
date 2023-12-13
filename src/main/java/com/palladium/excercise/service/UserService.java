54b23e6180adf353ac9e6aea48ecf99c4775ea6d
David Tega <davidogbodu3056@gmail.com>
2023-12-13 11:52:55 +0100

    Update UserService.java

--- src/main/java/com/palladium/excercise/service/UserService.java
+++ src/main/java/com/palladium/excercise/service/UserService.java
@@ -15,4 +15,14 @@ public class UserService {
     public List<Users> findAll(){
         return userRepository.findAll();
     }
+
+    public Users save(UserDto user){
+       Users user = Users.builder().
+                    username(user.getUserName())
+.email(user.getEmail())
+.phoneNumber(user.getPhoneNumber())
+.getDateCreated(user.getDateCreated())
+.status(user.getStatus())
+.build;
+return userRepository.save(user);
 }
